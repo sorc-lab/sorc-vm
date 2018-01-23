d2ba862e336b09aba91cb20ff7e2d89300bb5406
macfisher <macfisher01@gmail.com>
2018-01-22 21:00:39 -0700

    Added PHP 7 and Java 9 to provision.sh, called externally via Vagrant File

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,10 @@
 Vagrant.configure(2) do |config|
 	config.vm.box = "ubuntu/xenial64"
-    config.vm.provider "virtualbox" do |vb|
-		vb.memory = "1524"
+	config.vm.provider "virtualbox" do |vb|
+		vb.name = "sorc-vm"
+		vb.memory = 1524
+		vb.cpus = 2
 	end
+
+	config.vm.provision "shell", path: "provision.sh"
 end
--- provision.sh
+++ provision.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# $HOME/../../Program\ Files/Oracle/VirtualBox/VBoxManage.exe list ostypes
-#~/../../"Program Files"/Oracle/VirtualBox/VBoxManage.exe list ostypes
+apt-get update -y && apt-get upgrade -y
 
-VM='Ubuntu-16.04.2-64bit'
-~/../../Program\ Files/Oracle/VirtualBox/VBoxManage.exe createhd --filename $VM.vdi --size 32768
-#~/../../Program\ Files/Oracle/VirtualBox/VBoxManage.exe createvm --name $VM --ostype "Ubuntu_64" --register
+# install PHP 7
+sudo apt-get -y install php7.0 php7.0-curl php7.0-intl php7.0-gd php7.0-mcrypt php7.0-iconv php7.0-xsl php7.0-mbstring php7.0-zip php7.0-pdo php7.0-xml php7.0-json php7.0-mysqli php7.0-mysql php7.0-xmlwriter php7.0-xmlreader php7.0-soap php7.0-mysql libapache2-mod-php7.0
+php -v
+
+# install Java 9
+add-apt-repository ppa:webupd8team/java
+apt-get update -y
+apt install oracle-java9-installer
+apt install oracle-java9-set-default
+javac -version
+
